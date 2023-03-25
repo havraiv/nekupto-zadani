e78a906727698112986985b75407b1d75f212f51
Iva Havranová <havranova.i@seznam.cz>
2023-03-25 12:20:59 +0100

    Druhé zadání hotovo

--- index.js
+++ index.js
@@ -29,3 +29,18 @@ button2.classList.remove('btn-primary');
 const thirdProduct = document.querySelector('#product3');
 const cardElm3 = thirdProduct.querySelector('h2');
 cardElm3.classList.toggle('text-center');
+
+const product1content = `
+<img class="card-img-top" src="${product1.image}" alt="Card image cap">
+<div class="card-body">
+  <h2 class="card-title">${product1.name}</h2>
+  <p class="card-text">${product1.description}</p>
+</div>
+<button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+
+// Do nějaké proměnné si uložte element prvního produktu a nastavte jeho innerHTML na váš sestavený řetězec. Tímto na stránku vložíme obrázek prvního produktu.
+
+// Opakujte tento postup pro všechny ostatní produkty. Na konci byste měli dospět do stavu, kdy soubor index.html vůbec neosahuje vnitřek karet pro jednotlivé produkty. Jejich obsah je celý vyroben JavaScriptem z připravených dat.
+
+firstProduct.innerHTML = product1content;
